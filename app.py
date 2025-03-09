ee64d2dbb83a379250143dfe22f0d099a9bae4e8
akilachamathha <akilachamathha@gmail.com>
2025-03-09 22:54:10 +0530

    Refactor loan prediction logic and update model input parameters

--- app.py
+++ app.py
@@ -11,21 +11,22 @@ taxable_income = st.number_input('Monthly TaxableIncome (LKR)', 0.00, help='Ente
 nontaxable_income = st.number_input('Monthly NontaxableIncome (LKR)', 0.00, help='Enter your Monthly Nontaxable Income')
 total_deduction = st.number_input('Monthly TotalDeduction (LKR)', 0.00, help='Enter your Monthly Total Deduction')
 withholding_tax = st.number_input('Monthly WithholdingTax (LKR)', 0.00, help='Enter your Monthly Withholding Tax')
-net_worth = st.number_input('NetWorth (LKR)', 0.00, help='Enter your Net Worth')
-previous_loan_amount = st.number_input('PreviousLoanAmount (LKR)', 0.00, help='Enter the Previous Loan Amount')
 loan_duration = st.number_input('Loan Duration (Month)', 0, help='Enter the Loan Duration in Months')
 payment_history = st.number_input('Payment History (Month)', 0, help='Enter the Payment History in Months')
-loan_interest = st.number_input('LoanInterest (%)', 0.00, 100.00, help='Enter the Loan Interest Rate')
 
 gross_income = monthly_gross_income * 12
-net_pay = monthly_gross_income - withholding_tax - total_deduction
+remaining_loan_duration = loan_duration - payment_history
 
-entered_data = np.array([[gross_income, monthly_gross_income, taxable_income, nontaxable_income, total_deduction, withholding_tax, net_pay, net_worth, previous_loan_amount, loan_duration, payment_history, loan_interest]])
+if(monthly_gross_income == 0):
+    debt_income_ratio = 0
+else:
+    debt_income_ratio = total_deduction/monthly_gross_income
 
+entered_data = np.array([[gross_income, monthly_gross_income, taxable_income, nontaxable_income,debt_income_ratio, withholding_tax,remaining_loan_duration]])
 
 if(st.button('Predict')):
-    if(gross_income<=0 or net_worth<=0):
-        st.error('Please enter valid values. Gross Income and Net Worth should be greater than 0')
+    if(monthly_gross_income<=0):
+        st.error('Please enter valid values. Gross Income should be greater than 0')
     else:
         # Load the trained model
         model = joblib.load('random_forest_model.pkl')
